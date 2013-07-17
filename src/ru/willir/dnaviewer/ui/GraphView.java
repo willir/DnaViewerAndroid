e77f7a17dd13517756ebf3ba518f2451a796cb9e
Anton Rapetov <willir29@yandex.ru>
2013-07-17 20:40:10 +0400

    Correct some warning

--- src/ru/willir/dnaviewer/ui/GraphView.java
+++ src/ru/willir/dnaviewer/ui/GraphView.java
@@ -15,7 +15,6 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.FrameLayout;
 import android.view.View.OnLongClickListener;
 
 public class GraphView extends View implements OnLongClickListener {
